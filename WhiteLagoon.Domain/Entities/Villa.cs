ba54c11bcab102b9151192090982663a00d805ba
nmrvi <nmrvi@DESKTOP-GS7R98Q>
2024-02-21 14:07:03 +0000

    Pagina Home; HomeVM

--- WhiteLagoon.Domain/Entities/Villa.cs
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -13,19 +14,22 @@ namespace WhiteLagoon.Domain.Entities
         public string? Description { get; set; }
 
         [Display(Name = "Price per night")]
-        [Range(10,10000)]
+        [Range(10, 10000)]
         public double Price { get; set; }
         public int Sqft { get; set; }
 
-        [Range(1,10)]
+        [Range(1, 10)]
         public int Occupancy { get; set; }
 
-        [NotMapped] 
+        [NotMapped]
         public IFormFile? Image { get; set; }
 
         [Display(Name = "Image Url")]
         public string? ImageUrl { get; set; }
         public DateTime? CreatedDate { get; set; }
         public DateTime? UpdatedDate { get; set; }
+
+        [ValidateNever]
+        public IEnumerable<Amenity> VillaAmenity { get; set; } = null!;
     }
 }
