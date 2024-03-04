24dae4adc07399a7ed1766f1b4a0f891ea07a9ff
nmrvi <nmrvi@DESKTOP-GS7R98Q>
2024-03-04 13:07:34 +0000

    Registo e Login

--- WhiteLagoon.Web/Controllers/AccountController.cs
+++ WhiteLagoon.Web/Controllers/AccountController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using WhiteLagoon.Application.Common.Interfaces;
 using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Web.ViewModels;
 
 namespace WhiteLagoon.Web.Controllers
 {
@@ -10,12 +12,12 @@ namespace WhiteLagoon.Web.Controllers
         private readonly IUnitOfWork _IUnitOfWork;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
-        private readonly RoleManager<IdentityUser> _roleManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-        public AccountController(IUnitOfWork iUnitOfWork, 
-            UserManager<ApplicationUser> userManager, 
-            SignInManager<ApplicationUser> signInManager, 
-            RoleManager<IdentityUser> roleManager)
+        public AccountController(IUnitOfWork iUnitOfWork,
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            RoleManager<IdentityRole> roleManager)
         {
 
             _IUnitOfWork = iUnitOfWork;
@@ -24,9 +26,35 @@ namespace WhiteLagoon.Web.Controllers
             _roleManager = roleManager;
         }
 
-        public IActionResult Index()
+        public IActionResult Login(string returnUrl = null!)
         {
+            returnUrl ??= Url.Content("~/");
+
+            LoginVM loginVM = new()
+            {
+                RedirectUrl = returnUrl,
+            };
+
             return View();
+        }
+
+        public IActionResult Register()
+        {
+            if (!_roleManager.RoleExistsAsync("Admin").GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole("Admin")).Wait();
+                _roleManager.CreateAsync(new IdentityRole("Customer")).Wait();
+            }
+
+            RegisterVM registerVM = new()
+            {
+                RoleList = _roleManager.Roles.Select(_ => new SelectListItem
+                {
+                    Text = _.Name,
+                    Value = _.Name
+                })
+            };
+            return View(registerVM);
         }
     }
 }
